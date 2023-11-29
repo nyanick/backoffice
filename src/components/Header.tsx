663f5adcdf4e3f8660e86b7d6edc6488cb5e39a1
nyanick <yanicknde@gmail.com>
2023-11-29 02:18:24 +0100

    Added logout button in Admin backoffice

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -33,7 +33,7 @@ export const Header: React.FC<Props> = ({}) => {
                     <a href="#" className="nav-link">Support Technique</a>
                 </li>
             </ul>
-            {user && (
+            {(user || 1 ) && (
                 <ul
                     className="navbar-nav ml-auto flex items-center space-x-2 mr-2 cursor-pointer text-gray-500 hover:text-gray-700"
                     onClick={logout}>
